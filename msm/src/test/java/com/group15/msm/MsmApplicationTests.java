293365b55332df649947e0d9a57c6e5e1476b308
laviet <lavanvietk59@gmail.com>
2019-11-12 10:07:29 +0700

    add project

---
+++ msm/src/test/java/com/group15/msm/MsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group15.msm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
